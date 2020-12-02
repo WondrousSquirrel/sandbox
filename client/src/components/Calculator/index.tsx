6190f5b44dddc359bb7dd37dc6009f627025b48b
alexander <alexander@hack.com>
2020-12-02 15:35:00 +0200

    add functions to calculator

--- client/src/components/Calculator/index.tsx
+++ client/src/components/Calculator/index.tsx
@@ -4,56 +4,210 @@ import ThemeContext from '../theme';
 import './calculator.scss'
 import Button from './Button';
 
+type Operator = '+' | '-' | '*' | '/';
+
 const Calculator: FunctionComponent = () => {
 
     const theme = useContext(ThemeContext);
-    const [display, setDisplay] = useState([0]);
+    const [display, setDisplay] = useState<string>('0') // правая часть выражения
+    const [memory, setMemory] = useState<number>(0)
+    // не позволяет установить оператор с левой стороны операнда
+    const [waitOperand, setWaitForOperand] = useState<boolean>(true) 
+    // записиываем выбранный оператор и выполняем указанную операцию
+    const [pendingOperator, setPendingOperator] = useState<Operator>()
+    const [result, setResult] = useState<number>(0) // ялвяется левой частью выражения
+    // переключаем операции на работу с процентами
+    const [percentMode, setPercentMode] = useState<boolean>(false);
+    const percentage = () => {
+        setPercentMode(!percentMode);
+    }
 
-    const handleClick = () => {
-        console.log('button')
+    const calculate = (rightOperand: number, pendingOperator: Operator): boolean => {
+        let newResult = result;
+        let convertToPercent = result / 100 * rightOperand;
+        // выбераем необходимую операцию
+        switch (pendingOperator) {
+            case '+':
+                // проверка на работу с процентами
+                percentMode ? newResult +=  convertToPercent: newResult += rightOperand
+                break
+            case '-':
+                percentMode ? newResult -= convertToPercent : newResult -= rightOperand
+                break
+            case '*':
+                percentMode ? newResult *= convertToPercent : newResult *= rightOperand
+                break
+            case '/':
+                if (rightOperand === 0) {
+                    return false
+                }
+                percentMode ? newResult /= convertToPercent : newResult /= rightOperand
+                break;
+        }
+
+        console.log('before setting ' + newResult)
+        setResult(newResult)
+        setDisplay(newResult.toString().slice(0, 15))
+        return true
+    }
+
+    const addNumber = (digit: Number) => {
+        // ставим цифру
+        let newDisplay = display
+
+        // если экран пуст и выбрано число 0 или длина более 15 символов - ничего не делаем
+        if ((display === '0' && digit === 0) || display.length > 15) {
+          return
+        }
+        // после добавления числа позволяем выбирать операцию
+        if (waitOperand) {
+          newDisplay = ''
+          setWaitForOperand(false)
+        }
+    
+        if (display !== '0') {
+          newDisplay = newDisplay + digit.toString()
+        } else {
+          newDisplay = digit.toString()
+        }
+    
+        setDisplay(newDisplay)
     }
 
 
-    const addNumber = (value: any) => {
-        setDisplay([...display, value]);
+    const letDot = () => {
+        let newDisplay = display
+
+        if (waitOperand) {
+            newDisplay = '0'
+        }
+
+        if (newDisplay.indexOf('.') === -1) {
+            newDisplay = newDisplay + '.'
+        }
+
+        setDisplay(newDisplay)
+        setWaitForOperand(false)
     }
 
-    const calculate = () => {
-        const expression = [...display].join(' ');
-        console.log(expression);
+
+    const addOperator = (operator: Operator) => {
+        const operand = Number(display)
+
+        // если выбран оператор выполняем операцию
+        if (typeof pendingOperator !== 'undefined' && !waitOperand) {
+            if (!calculate(operand, pendingOperator)) { // в случае ошибки выходим из функции
+                return
+            }
+        } else { // или записываем число в левую часть выражения
+            setResult(operand)
+        }
+
+        setPendingOperator(operator) // записываем опирацию
+        setWaitForOperand(true)
+    }
+
+
+
+    const changeSign = () => {
+        // меняем знак числа
+        const value = Number(display)
+
+        if (value > 0) {
+            setDisplay('-' + display)
+        } else if (value < 0) {
+            setDisplay(display.slice(1)) // отсекаем перый индекс правого выражения
+        }
+    }
+
+    const equal = () => {
+        const operand = Number(display)
+
+        if (typeof pendingOperator !== 'undefined' && !waitOperand) {
+            if (!calculate(operand, pendingOperator)) {
+                return
+            }
+
+            setPendingOperator(undefined)
+        } else {
+            setDisplay(operand.toString())
+        }
+
+        setResult(operand)
+        setWaitForOperand(true)
+        console.log('result ' + result)
+    }
+
+
+    const allClear = () => {
+        // чистка всех данных
+        setMemory(0)
+        setResult(0)
+        setPendingOperator(undefined)
+        setDisplay('0')
+        setWaitForOperand(true)
+    }
+
+    const memoryRecall = () => {
+        // получаем значение из памяти
+        setDisplay(memory.toString())
+        setWaitForOperand(true)
+    }
+
+    const memoryClear = () => {
+        setMemory(0)
+        setWaitForOperand(true)
+    }
+
+    const addToMemory = () => {
+        setMemory(memory + Number(display))
+        setWaitForOperand(true)
+    }
+
+    const remooveFromMemory = () => {
+        setMemory(memory - Number(display))
+        setWaitForOperand(true)
     }
 
     return (
         <div className='centered'>
 
             <div className='calculator' style={{ backgroundColor: theme.secondColor, borderColor: theme.borderColor }}>
-                <div className='display'><p>{display.map(c => c)}</p></div>
+                <div className='display'>
+                    <div className='memory' style={{ color: theme.color2 }}>
+                        <p>{ percentMode ? '%': null}</p>
+                        <p>{memory === 0 ? null : memory}</p>
+                    </div>
+                    <div className='input'>
+                        <span>{display.length > 0 ? display : 0}</span>
+                    </div>
+                </div>
                 <div className="grid">
-                    <Button text='AC' backgroundColor={theme.buttonColor} onClick={handleClick} />
-                    <Button text='+/-' backgroundColor={theme.buttonColor} onClick={handleClick} />
-                    <Button text='%' backgroundColor={theme.buttonColor} onClick={handleClick} />
-                    <Button text='/' backgroundColor={theme.buttonColor2} onClick={handleClick} />
-                    <Button text='mc' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='mr' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='m-' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='m+' backgroundColor={theme.buttonColor2} onClick={handleClick} />
+                    <Button text='AC' backgroundColor={theme.buttonColor} onClick={allClear} />
+                    <Button text='+/-' backgroundColor={theme.buttonColor} onClick={changeSign} />
+                    <Button text='%' backgroundColor={theme.buttonColor} onClick={percentage} />
+                    <Button text='/' backgroundColor={theme.buttonColor2} onClick={() => addOperator('/')} />
+                    <Button text='mc' backgroundColor={theme.buttonColor3} onClick={memoryClear} />
+                    <Button text='mr' backgroundColor={theme.buttonColor3} onClick={memoryRecall} />
+                    <Button text='m-' backgroundColor={theme.buttonColor3} onClick={remooveFromMemory} />
+                    <Button text='m+' backgroundColor={theme.buttonColor2} onClick={addToMemory} />
                     <Button text='7' backgroundColor={theme.buttonColor3} onClick={() => addNumber(7)} />
                     <Button text='8' backgroundColor={theme.buttonColor3} onClick={() => addNumber(8)} />
                     <Button text='9' backgroundColor={theme.buttonColor3} onClick={() => addNumber(9)} />
-                    <Button text='*' backgroundColor={theme.buttonColor2} onClick={() => addNumber('*')} />
-                    <Button text='4' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='5' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='6' backgroundColor={theme.buttonColor3} onClick={handleClick} />
-                    <Button text='-' backgroundColor={theme.buttonColor2} onClick={handleClick} />
+                    <Button text='*' backgroundColor={theme.buttonColor2} onClick={() => addOperator('*')} />
+                    <Button text='4' backgroundColor={theme.buttonColor3} onClick={() => addNumber(4)} />
+                    <Button text='5' backgroundColor={theme.buttonColor3} onClick={() => addNumber(5)} />
+                    <Button text='6' backgroundColor={theme.buttonColor3} onClick={() => addNumber(6)} />
+                    <Button text='-' backgroundColor={theme.buttonColor2} onClick={() => addOperator('-')} />
                     <Button text='1' backgroundColor={theme.buttonColor3} onClick={() => addNumber(1)} />
                     <Button text='2' backgroundColor={theme.buttonColor3} onClick={() => addNumber(2)} />
                     <Button text='3' backgroundColor={theme.buttonColor3} onClick={() => addNumber(3)} />
-                    <Button text='+' backgroundColor={theme.buttonColor2} onClick={() => addNumber('+')} />
+                    <Button text='+' backgroundColor={theme.buttonColor2} onClick={() => addOperator('+')} />
                 </div>
                 <div className="bottom-grid">
-                    <Button text='0' backgroundColor={theme.buttonColor3} className='double-sized' onClick={handleClick} />
-                    <Button text=',' backgroundColor={theme.buttonColor3} className='dot-btn' onClick={handleClick} />
-                    <Button text='=' backgroundColor={theme.buttonColor2} className='equal-btn' onClick={calculate} />
+                    <Button text='0' backgroundColor={theme.buttonColor3} className='double-sized' onClick={() => addNumber(0)} />
+                    <Button text=',' backgroundColor={theme.buttonColor3} className='dot-btn' onClick={letDot} />
+                    <Button text='=' backgroundColor={theme.buttonColor2} className='equal-btn' onClick={equal} />
 
                 </div>
             </div>
--- client/src/components/theme.ts
+++ client/src/components/theme.ts
@@ -6,6 +6,7 @@ export const themes = {
         'secondColor': '#0A0A0A',
         'borderColor': '#332F37',
         'color': '#F7F7F2',
+        'color2': '#E9ECF580',
         'buttonColor': '#A3A3A3',
         'buttonColor2': '#FA9961',
         'buttonColor3': '#303136'
@@ -16,6 +17,7 @@ export const themes = {
         'borderColor': '#EDE8EB',
         'buttonColor': '#DBE0E6',
         'color': '#1F1F1F',
+        'color2': '#24203880',
         'buttonColor2': '#FA9961',
         'buttonColor3': '#C2C2C2'
     }
